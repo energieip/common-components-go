42fdc8bf732ea73eabcdee8cea630497e6453bf4
Amandine Drebes <amandine@energie-ip.com>
2019-07-11 11:54:42 +0200

    Update Switch CDC

--- pkg/dswitch/switch.go
+++ pkg/dswitch/switch.go
@@ -24,6 +24,7 @@ type Switch struct {
 	IsConfigured          *bool   `json:"isConfigured"`
 	FriendlyName          string  `json:"friendlyName"`
 	DumpFrequency         int     `json:"dumpFrequency"`
+	Profil                string  `json:"profil"` //I/O card configuration none/puls
 	Label                 *string `json:"label,omitempty"`
 }
 
@@ -45,7 +46,6 @@ type SwitchConfig struct {
 	WagosSetup    map[string]dwago.WagoSetup     `json:"wagosSetup"`
 	WagosConfig   map[string]dwago.WagoConf      `json:"wagosConfig"`
 	Users         map[string]duser.UserAccess    `json:"users"`
-	Label         *string                        `json:"label,omitempty"`
 }
 
 //SwitchStatus description
@@ -62,7 +62,20 @@ type SwitchStatus struct {
 	Groups        map[int]dgroup.GroupStatus       `json:"groups"`
 	ClusterBroker map[string]SwitchCluster         `json:"clusterBroker"`
 	Users         map[string]duser.UserAccess      `json:"users"`
-	Label         *string                          `json:"label,omitempty"`
+	StatePuls1    int                              `json:"statePuls1"`
+	StatePuls2    int                              `json:"statePuls2"`
+	StatePuls3    int                              `json:"statePuls3"`
+	StatePuls4    int                              `json:"statePuls4"`
+	StatePuls5    int                              `json:"statePuls5"`
+	StateBaes     int                              `json:"stateBaes"`
+	LedsPower     int64                            `json:"ledsPower"`
+	BlindsPower   int64                            `json:"blindsPower"`
+	HvacsPower    int64                            `json:"hvacsPower"`
+	TotalPower    int64                            `json:"totalPower"`
+	HvacsEnergy   int64                            `json:"hvacsEnergy"`
+	LedsEnergy    int64                            `json:"ledsEnergy"`
+	BlindsEnergy  int64                            `json:"blindsEnergy"`
+	TotalEnergy   int64                            `json:"totalEnergy"`
 }
 
 type SwitchCluster struct {
